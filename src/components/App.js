d3c7df3983090a12494e38cd180b0f5a5cd21763
arjundevlop <arjun.tomar45@gmail.com>
2023-04-25 11:16:02 +0530

    inputValue

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [inputValue, setinputValue] = useState("");
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent"><h1>Parent Component</h1>{inputValue}
+    <div className="child">
+    <h2>Child Component</h2>
+      <input type="text" value={inputValue} onChange={(e)=>{setinputValue(e.target.value)}} />
+    </div>
+        
     </div>
   )
 }
